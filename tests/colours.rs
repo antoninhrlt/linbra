c4712148c4f76471a6e71c34c796f46f77b18233
antoninhrlt <antonherault@gmail.com>
2023-03-12 03:08:17 +0100

    Tests for hex to rgba conversions and conflicts

---
+++ tests/colours.rs
@@ -0,0 +1,26 @@
+// This file is part of "linbra"
+// Under the MIT License
+// Copyright (c) 2023 Antonin Hérault
+
+#[test]
+fn no_implementation() {
+    use linbra::vectors::Vector3;
+ 
+    let colour: Vector3<u8> = [255, 100, 100].into();
+ 
+    let red = colour[0];
+    assert_eq!(255, red);
+}
+
+#[test]
+fn implementation() {
+    use linbra::{
+        vectors::Vector3,
+        colours::RGB,
+    };
+ 
+    let colour: Vector3<u8> = [255, 100, 100].into();
+ 
+    let red = colour.r();
+    assert_eq!(255, red);
+}
---
+++ tests/conflicts.rs
@@ -0,0 +1,35 @@
+// This file is part of "linbra"
+// Under the MIT License
+// Copyright (c) 2023 Antonin Hérault
+
+#[test]
+fn same_getters() {
+    use linbra::{
+        colours::RGB,
+        points::{ Point2, Point3 },
+        vectors::Vector3,
+    };
+
+    let vec3 = Vector3::<u8>::new([10, 5, 2]);
+    assert_eq!(10, vec3.x());
+    assert_eq!(5, vec3.y());
+    assert_eq!(2, vec3.z());
+    
+    assert_eq!(10, vec3.r());
+}
+
+#[test]
+fn same_into() {
+    use linbra::{
+        colours::RGB,
+        points::{ Point2, Point3 },
+        vectors::Vector3,
+    };
+
+    let vec3: Vector3<u8> = [10, 5, 2].into();
+    assert_eq!(10, vec3.x());
+    assert_eq!(5, vec3.y());
+    assert_eq!(2, vec3.z());
+    
+    assert_eq!(10, vec3.r());
+}
