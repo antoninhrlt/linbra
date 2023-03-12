b40fb9c5dc08fff720339311e25a475d7f5541f3
antoninhrlt <antonherault@gmail.com>
2023-03-12 02:53:14 +0100

    Crate root with the modules and the description

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 // This file is part of "linbra"
 // Under the MIT License
 // Copyright (c) 2023 Antonin Hérault
+
+//! Easily do linear algebra in game development, graphics and other sort of 
+//! calculations using vectors and matrices.
+
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+
+pub mod colours;
+pub mod points;
+pub mod vectors;
